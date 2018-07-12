b2a07bbd50c0ab028cd245c8907e072d8d7031a6
Jordan Parker <parkerj4321@gmail.com>
2018-07-12 08:59:28 -0500

    The homework on 'Naughty Strings and Fruits'

---
+++ Homework1.py
@@ -0,0 +1,10 @@
+# =====HomeWork 1 =======
+
+# x = 'This isn't a proper string'
+# print(x)
+
+# =======HomeWork 2 =======
+
+x = ["apples", "bananas", "carrots"]
+print(x)
+print("Mason likes to eat", x[0], x[1], "and", x[2], ".")
